bf6f7355baac50436de8bafe06f3e5db980c844e
Lin <lin2700166386@163.com>
2023-02-23 16:31:11 +0800

    :P

---
+++ CalamityCNPalyer.cs
@@ -0,0 +1,19 @@
+using CalamityCN;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Chat;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityCN
+{
+    public class CalamityCNPlayer : ModPlayer
+    {
+        public override void OnEnterWorld(Player player)
+        {
+            Color c = Color.Orange;
+            Main.NewText("额外备注：灾厄更新后天蓝矿的生成由地下转为在空岛中生成。", c);
+        }
+    }
+}
