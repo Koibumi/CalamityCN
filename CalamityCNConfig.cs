54cf5be3ff675f7f1ee3d99adeea8f568ac0371c
Furgo <furgos@qq.com>
2023-12-17 00:58:00 +0800

    简繁转换：初测通过，文件暂未转为繁体 TODO：转换文件，多次测试 （文件夹结构zhHans/zh-Hans是因为tML检测语言的关键词"zh-Hans"必须在hjson文件的上一级）

--- CalamityCNConfig.cs
+++ CalamityCNConfig.cs
@@ -1,5 +1,6 @@
           using System.ComponentModel;
-using Terraria.ModLoader.Config;
+          using CalamityCN.Systems;
+          using Terraria.ModLoader.Config;
 
 namespace CalamityCN
 {
@@ -13,14 +14,12 @@ namespace CalamityCN
         {
             Instance = this;
         }
-        /*
+        
         [ReloadRequired]
-        [Header("繁简转换")]
-        [Label("将大部分简体转化为繁体（仅包含灾厄本体）")]
-        [Tooltip("在简体中文-繁体中文（香港）-繁体中文（台灣）之间切换\n警告：不支持Mac系统，切换会报错！")]
-        [DefaultValue(0)]
-        public Systems.zhCNLang Lang;
-        */
+        [Header("Conversion")]
+        [DefaultValue(zhLang.zh)]
+        public zhLang Lang;
+        
         [ReloadRequired]
         [Header("CNinfernum")]
         public bool InfernumCN;
