3a118c0637662106bc216cb07a292e153f02563c
Listentowind <2926669682@qq.com>
2026-06-12 10:06:14 +0800

    2.2(v1)

--- CalamityCNPlayer.cs
+++ CalamityCNPlayer.cs
@@ -15,9 +15,9 @@ namespace CalamityCN
 
         public override void OnEnterWorld()
         {
-            // Main.NewText("感谢使用灾厄维基汉化组的灾厄汉化补丁！为了适配灾厄模组的更新，我们发布了这个临时版本以修复可能的错误。", Color.Orange);
-            // Main.NewText("由于准备仓促，部分内容可能未翻译或不够准确，还请您理解，我们会在后续更新中改进，无需反馈相关问题。", Color.Orange);
-            // Main.NewText("正式版发布后，这些内容将得到更新。感谢您的支持！", Color.Orange);
+            Main.NewText("感谢使用灾厄维基汉化组的灾厄汉化补丁！为了适配灾厄模组的更新，我们发布了这个临时版本以修复可能的错误。", Color.Orange);
+            Main.NewText("由于准备仓促，部分内容可能未翻译或不够准确，还请您理解，我们会在后续更新中改进，无需反馈相关问题。", Color.Orange);
+            Main.NewText("正式版发布后，这些内容将得到更新。感谢您的支持！", Color.Orange);
         }
 
     }
