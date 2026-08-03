e5816b305cdbe24ccd2cf26d2c69c866d485bb07
DimHaze <3815798709@qq.com>
2026-08-04 03:51:42 +0800

    将汉化组的提及从归一心元石移至Thank You
    如有不妥可单独回退

--- Translations/ItemGlobalTrans.cs
+++ Translations/ItemGlobalTrans.cs
@@ -1,40 +1,40 @@
-using System.Collections.Generic;
-using System.Linq;
-using Terraria;
-using Terraria.ModLoader;
+// using System.Collections.Generic;
+// using System.Linq;
+// using Terraria;
+// using Terraria.ModLoader;
 
-namespace CalamityCN.Translations
-{
-    public class ItemGlobalTrans : GlobalItem
-    {
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            return ModsCall.IsCN && ModsCall.Calamity != null;
-        }
+// namespace CalamityCN.Translations
+// {
+//     public class ItemGlobalTrans : GlobalItem
+//     {
+//         public override bool IsLoadingEnabled(Mod mod)
+//         {
+//             return ModsCall.IsCN && ModsCall.Calamity != null;
+//         }
 
-        private const string TranslatorTeamReferenceItem = "TheCommunity";
-        private static int TranslatorTeamReferenceItemType = -1;
+//         private const string TranslatorTeamReferenceItem = "TheCommunity";
+//         private static int TranslatorTeamReferenceItemType = -1;
 
-        public override void SetStaticDefaults()
-        {
-            TranslatorTeamReferenceItemType = -1;
-            Mod calamity = ModsCall.Calamity;
+//         public override void SetStaticDefaults()
+//         {
+//             TranslatorTeamReferenceItemType = -1;
+//             Mod calamity = ModsCall.Calamity;
 
-            if (calamity != null &&
-            calamity.TryFind(TranslatorTeamReferenceItem, out ModItem theItem))
-            {
-                TranslatorTeamReferenceItemType = theItem.Type;
-            }
-        }
+//             if (calamity != null &&
+//             calamity.TryFind(TranslatorTeamReferenceItem, out ModItem theItem))
+//             {
+//                 TranslatorTeamReferenceItemType = theItem.Type;
+//             }
+//         }
 
-        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
-        {
-            if (item.type == TranslatorTeamReferenceItemType)
-            {
-                var ThankYouTooltip = tooltips.FirstOrDefault(x => x.Name == "Tooltip3" && x.Mod == "Terraria");
-                if (ThankYouTooltip != null)
-                    ThankYouTooltip.OverrideColor = Main.DiscoColor;
-            }
-        }
-    }
-}
+//         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+//         {
+//             if (item.type == TranslatorTeamReferenceItemType)
+//             {
+//                 var ThankYouTooltip = tooltips.FirstOrDefault(x => x.Name == "Tooltip3" && x.Mod == "Terraria");
+//                 if (ThankYouTooltip != null)
+//                     ThankYouTooltip.OverrideColor = Main.DiscoColor;
+//             }
+//         }
+//     }
+// }
